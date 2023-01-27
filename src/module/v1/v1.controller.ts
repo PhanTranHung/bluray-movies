6e538be3c33e60b2977af3477396f2ae406dbe3b
phantranhung <zzsatthupk@gmail.com>
2023-01-27 20:30:21 +0700

    Versioning api

--- src/module/v1/v1.controller.ts
+++ src/module/v1/v1.controller.ts
@@ -1,10 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('/v1')
+@Controller('/')
 export class V1Controller {
   @Get('/')
   getHello(): string {
     return 'Hello from v1 api';
-    // return this.appService.findOne();
   }
 }
