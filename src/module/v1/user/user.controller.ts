62a311c355b4597d8eed38a0fe496cdb9a546a5e
phantranhung <zzsatthupk@gmail.com>
2023-01-27 18:08:11 +0700

    Implement OpenAPI

--- src/module/v1/user/user.controller.ts
+++ src/module/v1/user/user.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
+@ApiTags('Users')
 @Controller('/users')
 export class UserController {
   constructor(private readonly appService: UserService) {}
