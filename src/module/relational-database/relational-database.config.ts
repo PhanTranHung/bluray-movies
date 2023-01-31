b1daca001eb9ea8c2e8b854359deb974511d7a92
phantranhung <zzsatthupk@gmail.com>
2023-01-31 21:53:05 +0700

    Move MySQL config to env file

--- src/module/relational-database/relational-database.config.ts
+++ src/module/relational-database/relational-database.config.ts
@@ -1,14 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSourceOptions } from 'typeorm';
 
-// TODO: Move this config to env file
 export const dataSourceConfig: DataSourceOptions = {
   type: 'mysql',
-  host: 'bluray-movies.cnlvd2eru6yh.ap-southeast-1.rds.amazonaws.com',
-  port: 3306,
-  username: 'admin',
-  password: '123456789',
-  database: 'bluray_movies',
+  host: process.env.RDB_HOST,
+  port: +process.env.RDB_PORT,
+  username: process.env.RDB_USERNAME,
+  password: process.env.RDB_PASSWORD,
+  database: process.env.RDB_NAME,
   // TODO: Path to entities folder doesn't work. Fix this and remove autoLoadEntities option.
   entities: [`${__dirname}/entities/**/*.entity.ts`],
   migrations: [`${__dirname}/migrations/**/*.ts`],
