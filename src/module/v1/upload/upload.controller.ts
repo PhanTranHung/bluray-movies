1831067e38e03e7da57e1a20b8443606e4e20b7e
phantranhung <zzsatthupk@gmail.com>
2023-01-31 21:26:52 +0700

    Update Upload module

--- src/module/v1/upload/upload.controller.ts
+++ src/module/v1/upload/upload.controller.ts
@@ -1,15 +1,22 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { S3Service } from 'src/module/s3/s3.service';
+import { GetSignedUrlDto } from './dto/get-signed-url.dto';
 import { UploadVideoDto } from './dto/upload-video.dto';
 import { UploadService } from './upload.service';
 
 @ApiTags('Upload')
 @Controller('/upload')
 export class UploadController {
-  constructor(private readonly uploadService: UploadService) {}
+  constructor(private readonly uploadService: UploadService, private readonly s3Service: S3Service) {}
 
   @Post('/')
   uploadVideo(@Body() videoData: UploadVideoDto) {
     return this.uploadService.createVideo(videoData);
+  }
+
+  @Get('get-signed-url')
+  getSignedUrl(@Query() data: GetSignedUrlDto) {
+    return this.s3Service.signedUrl(data.file_name, data.folder);
   }
 }
--- src/module/v1/upload/upload.module.ts
+++ src/module/v1/upload/upload.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Video, VideoSchema } from 'src/module/mongodb/schemas/video.schema';
+import { S3Module } from 'src/module/s3/s3.module';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]), S3Module],
   providers: [UploadService],
   controllers: [UploadController],
 })
